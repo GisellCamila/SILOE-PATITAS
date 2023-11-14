053d4b1347d5115a980af9be98c804016324e5d3
GisellCamila <sfkl>
2023-11-13 22:37:22 -0500

    Se realizo javascript para ingresar como admin

--- ASSETS/JS/login.js
+++ ASSETS/JS/login.js
@@ -3,12 +3,12 @@ const formLogin = document.getElementById('login-form')
 if (formLogin) {
   formLogin.addEventListener('submit', (event) => {
     event.preventDefault()
-    const Username = event.target.elements.Username.value
+    const username = event.target.elements.username.value
     const password = event.target.elements.password.value
 
     // Username: Gisell; password: admin12345 //
 
-    if (Username === 'Gisell' && password === 'admin12345') {
+    if (username === 'Gisell' && password === 'admin12345') {
       localStorage.setItem('HAS_USER', 'true')
       window.location = '../VIEWS/admin.html'
       location = '../VIEWS/admin.html'
