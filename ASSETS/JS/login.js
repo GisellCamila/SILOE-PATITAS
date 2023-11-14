3387463c9b1cd76daaa4e09d398e974e1e8ed06d
GisellCamila <sfkl>
2023-11-13 19:33:21 -0500

    Se modifco el css y html de la pagina de Login y se realizo la de Signup; falta hacerlas responsive

---
+++ ASSETS/JS/login.js
@@ -0,0 +1,16 @@
+const formLogin = document.getElementById('login-form')
+
+if (formLogin) {
+  formLogin.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const Username = event.target.elements.Username.value
+    const password = event.target.elements.password.value
+
+    // Username: Gisell; password: admin12345 //
+
+    if (Username === 'admin@gmail.com' && password === 'admin12345') {
+      localStorage.setItem('HAS_USER', 'true')
+      window.location = '../VIEWS/admin.html'
+    }
+  })
+}
